8a911cc886b53bdaecf600f9e078aa5fb9b01eb8
Rafael G Francisco <rafael.dev@hotmail.com>
2020-06-06 11:33:51 -0300

    init

---
+++ modulo-1/main.tf
@@ -0,0 +1,19 @@
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 2.0"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-xpto"
+  acl    = "private"
+
+  tags = {
+    Name        = "My super bucket"
+    Environment = "Test"
+  }
+}
