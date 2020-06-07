c9b90a085761228259681cf1d750be4d20abaf59
Rafael G Francisco <rafael.dev@hotmail.com>
2020-06-07 12:05:38 -0300

    add creation to second bucket

--- modulo-3/main.tf
+++ modulo-3/main.tf
@@ -2,9 +2,17 @@ resource "random_id" "bucket" {
   byte_length = 8
 }
 
+resource "random_id" "bucket_2" {
+  byte_length = 4
+}
 
 module "bucket" {
   source = "./s3"
 
   name = "my-bucket-${random_id.bucket.hex}"
+
+module "bucket_2" {
+  source = "./s3"
+
+  name = "my-bucket-${random_id.bucket_2.hex}"
 }
